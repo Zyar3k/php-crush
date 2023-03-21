891e7f00e89edcff6d2f54b92f39a6a63ec14596
Arkadiusz Zygan <a.zygan@gmail.com>
2023-03-21 15:48:55 +0100

    Data types and variables

--- 01_output.php
+++ 01_output.php
@@ -1,10 +1,26 @@
-<?php
-echo "Hello3", 123, 10.5;
+ <?php
+    echo "Hello3", 123, 10.5;
 
-print 987;
+    print 987;
 
-print_r([1, 2, 3,]);
+    print_r([1, 2, 3,]);
 
-var_dump("Hello World!");
+    var_dump("Hello World!");
 
-var_export("Hello World!");
+    var_export("Hello World!");
+    ?>
+ <!DOCTYPE html>
+ <html lang="en">
+
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+ </head>
+
+ <body>
+     <h1><?php echo 'Post One'; ?></h1>
+ </body>
+
+ </html>
